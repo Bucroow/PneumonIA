8187774ab6d1d2f2c98dabe983ded525b8f059c5
SamuelH <73130274+Bucroow@users.noreply.github.com>
2022-10-24 12:39:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,44 +14,11 @@ from keras.optimizers import SGD
 img_width, img_height = 200, 200
 
 #functions
-num_classes=2
-model = tf.keras.Sequential([
-        layers.experimental.preprocessing.Rescaling(1./255),
-        layers.Conv2D(128,4, activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(64,4, activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32,4, activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(16,4, activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Flatten(),
-        layers.Dense(64,activation='relu'),
-        layers.Dense(num_classes, activation='softmax')
-        ])
-
-model.compile(optimizer='adam',
-                  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
-      metrics=['accuracy'])
-
-
-
-#interface
-
-st.write('HIET Samuel, POGGI Alexandre, BRUMELOT Noé - BIOINF _ 2022/2023')
-st.title('DataCamp')
-
-with st.sidebar:
-    st.write('Pneumonia is an infection that inflames the air sacs in one or both lungs. The air sacs may fill with fluid or pus (purulent material), causing cough with phlegm or pus, fever, chills, and difficulty breathing. A variety of organisms, including bacteria, viruses and fungi, can cause pneumonia. Pneumonia can range in seriousness from mild to life-threatening. It is most serious for infants and young children, people older than age 65, and people with health problems or weakened immune systems.\n\n ____________________\n\nThe objective of this app is to detect if a patient as a pneumonia thanks to a picture of his lungs.')
-
-
-
 # load and compile model previously made with the training.ipynb
 model = load_model('chest_images_vgg19.h5')
 model.compile(loss = "binary_crossentropy", 
               optimizer = SGD(lr=0.001, momentum=0.9), 
               metrics=["accuracy"])
-
 
 
 def modelPredict(image):
@@ -60,14 +27,32 @@ def modelPredict(image):
     img_to_predict = np.expand_dims(cv2.resize(image_to_predict,(200,200)), axis=0) 
     predict_x=model.predict(img_to_predict)
     classes_x=np.argmax(predict_x,axis=1)
-    print(classes_x[0])
-    st.image(image, width=500)
+    
     if classes_x[0] == 0:
-        st.balloons()
-        st.write("Prediction : Healthy")
+        st.header("Prediction : Healthy")
     elif classes_x[0] == 1 :
-        st.snow()
-        st.write("Prediction : Pneumonia")
+        st.header("Prediction : Pneumonia")
+
+    st.image(image, width=500)
+    
+#interface
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.write(' ')
+with col2:
+    st.write(' ')
+with col3:
+    st.image('LOGO_EFREI.png')
+
+st.title('Pneumonnia Detection Application')
+
+st.write('This tool will help you to discover if you have a pneumonia. Thanks to one of your chest X-ray, our system will make a prediction (92 percent of accuracy).')
+st.write(' ')
+
+with st.sidebar:
+    st.write('Pneumonia is an infection that inflames the air sacs in one or both lungs. The air sacs may fill with fluid or pus (purulent material), causing cough with phlegm or pus, fever, chills, and difficulty breathing. A variety of organisms, including bacteria, viruses and fungi, can cause pneumonia. Pneumonia can range in seriousness from mild to life-threatening. It is most serious for infants and young children, people older than age 65, and people with health problems or weakened immune systems.\n\n ____________________\n\nThe objective of this app is to detect if a patient as a pneumonia thanks to a picture of his lungs.')
+
 
 
 #download file
@@ -80,3 +65,7 @@ if uploaded_file is not None:
 
     modelPredict(uploaded_file)
 
+st.write(' ')
+st.write(' _______________________________________________________________________________ ')
+st.markdown('Web application developped by **HIET Samuel**, **POGGI Alexandre** & **BRUMELOT Noé**')
+st.markdown('M1.BIOINF, 2022-2023')
